a628dd708a5f3ed9f1986289fd4f3154f894b9dd
KirillNevish <kirillvish482@gmail.com>
2024-01-30 02:34:47 +0200

    HW_01

---
+++ hw_01/main.js
@@ -0,0 +1,9 @@
+let userNickname = "John";
+let userAge = 25;
+let userInformation = userNickname + userAge;
+console.log('Information =', userInformation);
+let userScore = 65;
+console.log('Score=', userScore)
+let firstPrompt = prompt('стоимость 1 товара 5+5=?');
+let secondPrompt = prompt('всего товаров 4+1=?');
+console.log(firstPrompt*secondPrompt)
