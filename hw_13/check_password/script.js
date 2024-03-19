ec455c55aa78cf02dc82f99b4068e1d03fcf3912
KirillNevish <kirillvish482@gmail.com>
2024-03-19 14:37:56 +0200

    HW_13

--- hw_13/check_password/script.js
+++ hw_13/check_password/script.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 
-    let searchRegExp = /[a-zA-Z]{8,20}[!@#$%^&*]{1,2}\d{1}/gi;
+    let searchRegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%&*]).{8,20}$/;
     
-    console.log(`Is password valid: ${searchRegExp.test('Loremsrrsm!9')}`);
+    console.log(`Is password valid: ${searchRegExp.test('Lorem9ssrrsm!')}`);
+    console.log(`Is password valid: ${searchRegExp.test('l%remIPs9uM')}`);
 }
